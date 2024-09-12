70aa64a50fe9fc718b064678142174740e110dc8
juni <sherophae312@gmail.com>
2024-09-12 20:56:41 +0900

    0911

---
+++ 0911_boj.py
@@ -0,0 +1,12 @@
+'''
+a, b = map(int,input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+'''
+word = list(input())
+word.append('??!')
+for i in word:
+    print(i, end='')
